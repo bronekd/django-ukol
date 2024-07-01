e76e9cc6f9603ddf474372b98c1a12490da22601
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-07-01 20:59:35 +0200

    Spuštěna první vlastní teplate mycity

--- city/city/views.py
+++ city/city/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the city index.")
+    return HttpResponse("Konečně jsem se spustil.")
 
-
+def mycity(request):
+    template = loader.get_template('mycity.html')
+    return HttpResponse(template.render())
 
